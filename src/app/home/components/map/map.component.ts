6ae65b6bff02678d58e9e00904a9bec614e07435
alvinosh <alvihysa89@gmail.com>
2021-05-19 14:49:14 +0200

    Added map component

--- src/app/home/components/map/map.component.ts
+++ src/app/home/components/map/map.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
+  private map;
 
-  constructor() { }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3,
+    });
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() {}
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.initMap();
+  }
 }
