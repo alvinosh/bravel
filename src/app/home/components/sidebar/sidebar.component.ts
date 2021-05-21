3a1ab63a89b918028e9e688e5036af7c1d07480f
alvinosh <alvihysa89@gmail.com>
2021-05-21 19:35:28 +0200

    added sidebar tabs

--- src/app/home/components/sidebar/sidebar.component.ts
+++ src/app/home/components/sidebar/sidebar.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Page } from 'src/app/shared/models/Page';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnChanges {
+  @Input() page: Page = Page.Back; // decorate the property with @Input(
 
-  constructor() { }
+  Page = Page;
+
+  isPage(x: Page): boolean {
+    return x === this.page;
+  }
+
+  constructor() {
+    console.log(this.page);
+  }
 
   ngOnInit() {}
 
+  ngOnChanges() {
+    console.log(this.page);
+  }
 }
